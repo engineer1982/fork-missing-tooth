3fd47ab452e4564af460b9ac6f95479e1c8a7a6f
engineer1982 <36931286+engineer1982@users.noreply.github.com>
2018-04-02 16:44:59 -0300

    Add files via upload

---
+++ loadpack.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import Image
+import glob
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import optimizers
+
+print("packages loaded")
+
+return "ok"
